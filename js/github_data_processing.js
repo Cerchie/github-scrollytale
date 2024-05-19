4b6c3ab9124a0664621a256f5ef3f025eeb10a68
Cerchie <luciacerchie@gmail.com>
2024-05-19 16:08:41 -0700

    last few methods

--- js/github_data_processing.js
+++ js/github_data_processing.js
@@ -23,11 +23,25 @@ class responseData {
         const stargazers_count = fullData.stargazers_count;
         return {stargazers_count, forks_count};
       }
+      async getLengthActive(owner, repo) {
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+        const fullData = await response.json();
+        const created_at = fullData.created_at;
+        const pushed_at = fullData.pushed_at;
+        return {created_at, pushed_at};
+      }
+      async getSize(owner, repo) {
+        //https://stackoverflow.com/questions/8646517/how-can-i-see-the-size-of-a-github-repository-before-cloning-it
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}`);
+        const fullData = await response.json();
+        const size = fullData.size;
+        return size;
+      }
 }
 
-const gettingData = new responseData;
+// const gettingData = new responseData;
 
 
-  (async () => {
-   console.log(await gettingData.getStargazersAndForks("apache", "flink"))
-  })();
+//   (async () => {
+//    console.log(await gettingData.getSize("apache", "flink"))
+//   })();
